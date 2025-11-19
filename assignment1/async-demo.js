3ceb3c51740ace6a9f7465ddc2d12a8ce30e503d
Angel Astorga <astorga_ms@hotmail.com>
2025-11-18 23:32:26 -0500

    working with nodejs basic tasks, async, global variables, callbacks

--- assignment1/async-demo.js
+++ assignment1/async-demo.js
@@ -1,16 +1,71 @@
-const fs = require('fs');
-const path = require('path');
+const fs = require("fs");
+const path = require("path");
 
+const MYPATH = path.join(__dirname, "sample-files", "sample.txt");
 
 // Write a sample file for demonstration
 
 // 1. Callback style
+fs.readFile(MYPATH, "utf8", (err, data) => {
+  if (err) {
+    console.log("error:", err);
+    return;
+  }
+  console.log(`Callback read: ${data}`);
+});
 
+// Callback hell example (test and leave it in comments):
+// here you can see how every nestetd callback create a triangular structure
+// fs.readFile(MYPATH, "utf8", (err, data) => {
+//   if (err) {
+//     console.log("error:", err);
+//     return;
+//   }
+//   console.log(data);
+//   fs.readFile(MY_SECOND_PATH,"utf8",(err, data)=>{
+//       if (err) {
+//         console.log("error:", err);
+//         return;
+//        }
+//       console.log("second file readed")
+//       fs.readFile(MY_THIRD_PATH, "utf8",(err,data)=>{
+//         if(err){
+//               console.log("error:", err);
+//         return;
+//       }
+//       console.log("third file readed")
+//     });
+//   });
+// });
 
-  // Callback hell example (test and leave it in comments):
+// 2. Promise style
 
+function promisifyMyFunction(myPath, encoding) {
+  return new Promise((resolve, reject) => {
+    fs.readFile(myPath, encoding, (err, data) => {
+      if (err) {
+        reject(err);
+      }
+      resolve(data);
+    });
+  });
+}
 
-  // 2. Promise style
+promisifyMyFunction(MYPATH, "utf8")
+  .then((data) => {
+    console.log(`Promise read: ${data}`);
+  })
+  .catch((err) => {
+    console.log(err);
+  });
 
-
-      // 3. Async/Await style
+// 3. Async/Await style
+async function myAsyncFunction() {
+  try {
+    const data = await promisifyMyFunction(MYPATH, "utf8");
+    console.log(`Async/Await read: ${data}`);
+  } catch (err) {
+    console.log(err);
+  }
+}
+myAsyncFunction();
--- assignment1/core-modules-demo.js
+++ assignment1/core-modules-demo.js
@@ -1,18 +1,33 @@
-const os = require('os');
-const path = require('path');
-const fs = require('fs');
+const os = require("os");
+const path = require("path");
+const fs = require("fs");
 
-const sampleFilesDir = path.join(__dirname, 'sample-files');
+const sampleFilesDir = path.join(__dirname, "sample-files");
 if (!fs.existsSync(sampleFilesDir)) {
   fs.mkdirSync(sampleFilesDir, { recursive: true });
 }
 
 // OS module
-
+console.log(`Platform: ${os.platform}`);
+console.log(`CPU: ${os.arch}`);
+console.log(`Total Memory: ${os.totalmem}`);
 
 // Path module
-
+const MY_NEW_PATH = path.join(__dirname, "sample-files", "demo.txt");
+console.log(`Joined path: ${MY_NEW_PATH}`);
 // fs.promises API
+const fsPromises = require("fs/promises");
 
+const doFileOperations = async () => {
+  try {
+    const fileHandle = await fsPromises.open(MY_NEW_PATH, "w+");
+    const { bytesWritten } = await fileHandle.write("Hello file", 0, "utf8");
+    const dataBuffer = await fileHandle.readFile({ encoding: "utf8" });
+    console.log(`fs.promises read: ${dataBuffer}`);
+  } catch (err) {
+    console.log("A error occurred.", err);
+  }
+};
 
+doFileOperations();
 // Streams for large files- log first 40 chars of each chunk
