ab223a44762090a34536bcdb287da6fc28f2876d
Angel Astorga <astorga_ms@hotmail.com>
2025-11-25 23:37:25 -0500

    working with http requests and express

---
+++ assignment2/events.js
@@ -0,0 +1,11 @@
+const EventEmitter = require("events");
+const emitter = new EventEmitter();
+
+emitter.on("time", (val) => {
+  console.log("Time received: " + val);
+});
+
+setInterval(() => {
+  const currentTime = new Date();
+  emitter.emit("time", currentTime);
+}, 5000);
