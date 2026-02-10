15c215ab7387f68d53aab802affb96f03b5ec19e
Angel Astorga <astorga_ms@hotmail.com>
2026-02-09 22:43:52 -0500

    fixing cookies lax

--- controllers/userController.js
+++ controllers/userController.js
@@ -21,7 +21,7 @@ const cookieFlags = (req) => {
   return {
     httpOnly: true,
     secure: process.env.NODE_ENV === "production", // only when HTTPS is available
-    sameSite: "Strict",
+    sameSite: "Lax",
   };
 };
 
